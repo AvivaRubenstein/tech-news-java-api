40a9c931d5c5aa716ecd1c1e6a85e93d0775053e
Aviva <avivadrubenstein@gmail.com>
2023-07-23 14:53:02 -0400

    practice with creating java classes, including constructor functions, getters/setters, and @Override annotations for common Object methods

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        int a = 9;
+        double b = 1.5;
+        double sum = a + b;
+        System.out.println(sum);
+        String c = "hello";
+        String d = c + " " +  a;
+        System.out.println(d);
+
+
+    }
+}
